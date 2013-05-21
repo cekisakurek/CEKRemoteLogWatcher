6bbfab15dc51e186adaa1380137d38589052c4c8
cekisakurek <cekisakurek@gmail.com>
2013-05-21 17:18:19 +0300

    Initial Commit

---
+++ CEKRemoteLogWatcher/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  CEKRemoteLogWatcher
+//
+//  Created by cihan emre kisakurek on 5/21/13.
+//  Copyright (c) 2013 cekware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
