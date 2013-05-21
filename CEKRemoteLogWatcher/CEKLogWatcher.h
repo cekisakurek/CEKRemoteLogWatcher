75375e86902d27651668209854bb5ae337cc7647
cekisakurek <cekisakurek@gmail.com>
2013-05-21 17:45:50 +0300

    first commit

---
+++ CEKRemoteLogWatcher/CEKLogWatcher.h
@@ -0,0 +1,13 @@
+//
+//  CEKLogWatcher.h
+//  CEKRemoteLogWatcher
+//
+//  Created by cihan emre kisakurek on 5/21/13.
+//  Copyright (c) 2013 cekware. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CEKLogWatcher : NSObject
+
+@end
---
+++ CEKRemoteLogWatcher/CEKLogWatcher.m
@@ -0,0 +1,13 @@
+//
+//  CEKLogWatcher.m
+//  CEKRemoteLogWatcher
+//
+//  Created by cihan emre kisakurek on 5/21/13.
+//  Copyright (c) 2013 cekware. All rights reserved.
+//
+
+#import "CEKLogWatcher.h"
+
+@implementation CEKLogWatcher
+
+@end
